429bd05804a30e831bdb24a51b85490b2b910c36
ignacio-leon-m <ig.leon@duocuc.cl>
2023-09-29 14:04:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'regristrAppTW',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
